722cf1bb1f2d43fedce7454bf9fec657d0e4c39d
Hoàn Nguyễn <nguyenduyhoan0401@gmail.com>
2025-04-01 12:50:11 +0700

    v0.1

---
+++ MechBot/src/main/java/hoannd/mech/bot/MechBot/MechBotApplication.java
@@ -0,0 +1,13 @@
+package hoannd.mech.bot.MechBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechBotApplication.class, args);
+	}
+
+}
---
+++ MechBot/src/test/java/hoannd/mech/bot/MechBot/MechBotApplicationTests.java
@@ -0,0 +1,13 @@
+package hoannd.mech.bot.MechBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
